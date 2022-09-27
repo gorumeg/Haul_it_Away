856c21207180aa4bcc1df60d6827cea97e8f8654
gorumeg <92520188+gorumeg@users.noreply.github.com>
2022-09-27 09:16:05 +0530

    Add files via upload

---
+++ socialDistanceAnalyzer/constants.py
@@ -0,0 +1,6 @@
+YOLOV3_LABELS_PATH = 'C:\\Users\\samri\\OneDrive\\Desktop\\object\\SocialDistancingAnalyzer\\yolov3\\coco.names'
+YOLOV3_CFG_PATH = 'C:\\Users\\samri\\OneDrive\\Desktop\\object\\SocialDistancingAnalyzer\\yolov3\\yolov3.cfg'
+YOLOV3_WEIGHTS_PATH = 'C:\\Users\\samri\\OneDrive\\Desktop\\object\\SocialDistancingAnalyzer\\yolov3\\yolov3.weights'
+VIDEO_PATH = 'C:\\Users\\samri\\OneDrive\\Desktop\\object\\SocialDistancingAnalyzer\\videos\\video1.mp4'
+OUTPUT_PATH = 'C:\\Users\\samri\\OneDrive\\Desktop\\object\\SocialDistancingAnalyzer\\output\\output.avi'
+SAFE_DISTANCE = 420
